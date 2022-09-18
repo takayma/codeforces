338e79668f5766885304ada7a84829f31a8f087b
Takayama <69343092+takayma@users.noreply.github.com>
2022-09-18 18:28:55 +0300

    Add files via upload

---
+++ 1726A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i = 0; i < n; ++i)
+            cin >> a[i];
+
+        
+    }
+}
